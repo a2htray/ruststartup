153d7a98531a2b4ca21fd9207d41d9cc952c41ea
a2htray <a2htray.yuen@gmail.com>
2024-06-06 14:19:27 +0800

    初始化学习项目 & 代码注释

---
+++ src/comment.rs
@@ -0,0 +1,17 @@
+pub fn line_comment() {
+    // 行注释
+    println!("单行注释");
+}
+
+pub fn block_comment() {
+    /*
+      块注释 1
+      块注释 2
+    */
+    println!("块注释");
+}
+
+/// 文档注释
+pub fn doc_comment() {
+    println!("文档注释");
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use ruststartup::comment;
+
+fn main() {
+    comment::line_comment();
+    comment::block_comment();
+    comment::doc_comment();
+}
